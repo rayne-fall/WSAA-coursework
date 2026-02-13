7129551a16eaebec4a05c0a0fb8c7b479ea2f129
Sylvia Chapman Kent <sylvia.kent.2.1@hotmail.com>
2026-02-13 13:48:45 +0000

    card draw now pulls 5 cards and checks for pairs triples and 5 cards of same suit, may not properly check for straights

--- assignments/assignment2-carddraw.py
+++ assignments/assignment2-carddraw.py
@@ -4,6 +4,7 @@
 
 import requests
 import json
+import itertools
 #get shuffled deck
 url1="https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
 response1=requests.get(url1)
@@ -11,15 +12,43 @@ deck=response1.json()
 #record deck id
 deck_id=deck["deck_id"]
 #draw 2 cards from shuffled deck
-url2=(f"https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count=2")
+url2=(f"https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count=5")
 response2=requests.get(url2)
 draw=response2.json()
-#record details of both cards [1]
+#record details of all cards [1]
 first_card=draw['cards'][0]
 second_card=draw['cards'][1]
+third_card=draw['cards'][2]
+fourth_card=draw['cards'][3]
+fifth_card=draw['cards'][4]
 #print value and suit of each card
-print(first_card["value"],first_card["suit"],second_card["value"],second_card["suit"])
-# to do: check if user has drawn pair triple straight or all same suit and congratulate them
-
+print(first_card["value"],first_card["suit"],second_card["value"],second_card["suit"],third_card["value"],third_card["suit"],fourth_card["value"],fourth_card["suit"],fifth_card["value"],fifth_card["suit"])
+card_values=[first_card["value"],second_card["value"],third_card["value"],fourth_card["value"],fifth_card["value"]]  
+# check if user's drawn five cards iif the same suit
+if first_card["suit"]==second_card["suit"]==third_card["suit"]==fourth_card["suit"]==fifth_card["suit"]:
+    print(f"Wow! You drew five {first_card["suit"]}!")
+# check if user drew a pair [2]
+for a,b in itertools.combinations(card_values, 2):
+    if a==b:
+        print(f"Whoa! You drew a pair of {a}s!")
+# check if user has drawn a triple
+for a,b,c in itertools.combinations(card_values, 3):
+    if a==b==c:
+        print(f"Nice! A triple! You drew three {a}s!")
+# convert face cards to their numerical values [3]
+for i in range(len(card_values)):
+    card_values[i]=11 if card_values[i]=="JACK" else card_values[i]
+for j in range(len(card_values)):
+    card_values[j]=12 if card_values[j]=="QUEEN" else card_values[j]
+for k in range(len(card_values)):
+    card_values[k]=13 if card_values[k]=="KING" else card_values[k]
+for l in range(len(card_values)):
+    card_values[l]=1 if card_values[l]=="ACE" else card_values[l]
+# check if user has drawn a straight
+for a,b,c,d,e in itertools.combinations(card_values, 5):
+    if a==(int(b)+1) and b==(int(c)+1) and c==(int(d)+1) and d==(int(e)+1):
+        print("Sweet! You drew a straight!")
 #References:
 #[1]https://www.geeksforgeeks.org/python/how-to-parse-nested-json-in-python/
+#[2]https://stackoverflow.com/questions/16603282/how-to-compare-each-item-in-a-list-with-the-rest-only-once
+#[3]https://stackoverflow.com/questions/44081148/python-replace-string-in-list-with-integer
